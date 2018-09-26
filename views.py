8da1ff38abbfe2638d250a70d48ec8c94007166a
yzq110u <1019493598@qq.com>
2018-09-26 14:59:16 +0800

    创建login视图

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-
+from dajngo.shortcuts import redirect
 
 def index(request):
 
 	return HttpResponse("index")
+
+def login(request):
+	return redirect("/index")
